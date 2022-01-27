8ae18e77243eed7454b37ff93d32bf7643fa7b7a
Bradley Wood <52896759+BradleyLWood@users.noreply.github.com>
2022-01-27 10:55:36 -0700

    Added Deno env and commented

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,6 +1,5 @@
+# Greeting
 set fish_greeting ""
-
-#set -gx TERM xterm-256color
 
 # Commands to run in interactive sessions can go here
 fish_vi_key_bindings
@@ -8,15 +7,18 @@ fish_user_vi_key_bindings
 
 # Aliases
 
+# vim
 alias v nvim
 
-#alias ls "ls -hpG --group-directories-first --color"
-#alias la "ls -A"
-#alias ll "ls -l"
+# Use ls
+#alias ls  "ls -hpG --group-directories-first --color"
+#alias la  "ls -A"
+#alias ll  "ls -l"
 #alias lla "la -A"
-alias ls "exa --icons --group-directories-first"
-alias la "ls -a"
-alias ll "ls -l"
+# Use exa
+alias ls  "exa --icons --group-directories-first"
+alias la  "ls -a"
+alias ll  "ls -l"
 alias lla "la -l"
 
 # Attach tmux to a session if one exists, otherwise creates one
@@ -36,33 +38,31 @@ alias r "ranger"
 # Pacman
 abbr -a p "sudo pacman"
 
-# dotfile git commands
-# use function in functions/dotfiles.fish to get working autocomplete
-#alias config 'git --git-dir=$HOME/.cfgrepo --work-tree=$HOME'
-
-# git abbreviations
-abbr -a gd "git difftool"
-abbr -a ga "git add"
-abbr -a gaa "git add --all ."
-abbr -a gbd "git branch -D"
-abbr -a gs "git status"
-abbr -a gc "git commit"
-abbr -a gca "git commit -a"
-abbr -a gm "git merge --no-ff"
-abbr -a gpt "git push --tags"
-abbr -a gp "git push"
-abbr -a grh "git reset --hard"
-abbr -a gb "git branch"
+# Git abbreviations
+abbr -a gd   "git difftool"
+abbr -a ga   "git add"
+abbr -a gaa  "git add --all ."
+abbr -a gbd  "git branch -D"
+abbr -a gs   "git status"
+abbr -a gc   "git commit"
+abbr -a gca  "git commit -a"
+abbr -a gm   "git merge --no-ff"
+abbr -a gpt  "git push --tags"
+abbr -a gp   "git push"
+abbr -a grh  "git reset --hard"
+abbr -a gb   "git branch"
 abbr -a gcob "git checkout -b"
-abbr -a gco "git checkout"
-abbr -a gba "git branch -a"
-abbr -a gl "git log --pretty=format:\"%Cgreen%h%Creset - %Cblue%an%Creset @ %ar : %s\""
-abbr -a gl2 "git log --pretty='format:%Cgreen%h%Creset %an - %s' --graph"
-abbr -a glv "git log --stat"
+abbr -a gco  "git checkout"
+abbr -a gba  "git branch -a"
+abbr -a gl   "git log --pretty=format:\"%Cgreen%h%Creset - %Cblue%an%Creset @ %ar : %s\""
+abbr -a gl2  "git log --pretty='format:%Cgreen%h%Creset %an - %s' --graph"
+abbr -a glv  "git log --stat"
 abbr -a gpom "git pull origin master"
 
+# Editor
 set -gx EDITOR nvim
 
+# Path
 set -gx PATH bin $PATH
 set -gx PATH ~/bin $PATH
 set -gx PATH ~/code/public_scripts $PATH
@@ -71,5 +71,11 @@ set -gx PATH ~/.yarn/bin $PATH
 set -gx PATH ~/.local/bin $PATH
 set -gx PATH /usr/local/bin $PATH
 
-set -g GOPATH ~/go
+# Deno
+set -gx DENO_INSTALL ~/.deno 
+set -gx PATH $DENO_INSTALL/bin $PATH
+
+# Go
+set -gx GOPATH ~/go
 set -gx PATH $GOPATH/bin $PATH
+
