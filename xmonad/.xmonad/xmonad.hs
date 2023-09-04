cc0abc36290ca4158c17512316e8f0f93ca6abc8
Bradley Wood <52896759+BradleyLWood@users.noreply.github.com>
2023-09-04 12:16:17 -0600

    minor xmonad changes

--- xmonad/.xmonad/xmonad.hs
+++ xmonad/.xmonad/xmonad.hs
@@ -8,9 +8,8 @@ import XMonad.Util.Run(spawnPipe, hPutStrLn)
 import XMonad.Util.EZConfig
 import XMonad.Util.Loggers
 import XMonad.Util.Ungrab
-
 import XMonad.Layout.Spacing
-import XMonad.Layout.NoBorders ( noBorders )
+--import XMonad.Layout.NoBorders ( noBorders )
 
 import XMonad.Hooks.EwmhDesktops
 
@@ -30,11 +29,20 @@ main = do
 			, clickJustFocuses   = myClickJustFocuses
             }
           `additionalKeysP`
-            [ ("M-S-z", spawn "xscreensaver-command -lock")
-            , ("M-S-=", unGrab *> spawn "scrot -s"        )
-            , ("M-p", spawn "rofi -show drun -modi")
-            , ("M-S-p", spawn "rofi -show window -modi")
-            , ("M-b", sendMessage ToggleStruts)
+            [ ("M-S-z"                     , spawn "xscreensaver-command -lock"               )
+            , ("M-S-="                     , unGrab *> spawn "scrot -s"                       )
+            , ("M-p"                       , spawn "rofi -show drun -modi"                    )
+            , ("M-S-p"                     , spawn "rofi -show window -modi"                  )
+            , ("M-b"                       , sendMessage ToggleStruts                         )
+            , ("M-S-t"                     , spawn "xterm"                                    )
+            -- FIXME: these are not working
+            --, ((0, xF86XK_AudioLowerVolume), spawn "pactl set-sink-volume @DEFAULT_SINK@ -5"  )
+            --, ((0, xF86XK_AudioRaiseVolume), spawn "pactl set-sink-volume @DEFAULT_SINK@ +5"  )
+            --, ((0, xF86XK_AudioMute)       , spawn "pactl set-sink-mute @DEFAULT_SINK@ toggle")
+            -- Switch w, e, r monitory switching keys
+            , ((m .|. modMask, key), screenWorkspace sc >>= flip whenJust (windows . f))
+                | (key, sc) <- zip [xK_w, xK_e, xK_r] [2,1,3]
+                , (f, m) <- [(W.view, 0), (W.shift, shiftMask)]
             ]
 
 --------------------------------------------------------------------------------
@@ -59,11 +67,11 @@ myManageHook = composeAll
 
 --------------------------------------------------------------------------------
 
-myLayout = avoidStruts $ noBorders Full ||| withSpaces tiled ||| tiled ||| withSpaces (Mirror tiled ) ||| Mirror tiled
+myLayout = avoidStruts $ withSpaces Full ||| Full ||| withSpaces tiled ||| withSpaces (Mirror tiled )
   where
     tiled   = Tall nmaster delta ratio
     nmaster = 1
-    ratio   = 1/2
+    ratio   = 3/5
     delta   = 3/100
-    withSpaces layout = spacingWithEdge 10 $ layout
+    withSpaces layout = spacingWithEdge 30 $ layout
 
