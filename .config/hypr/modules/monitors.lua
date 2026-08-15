358cb408b93c996f05667301b768b303e4070b01
Bradley Wood <52896759+BradleyLWood@users.noreply.github.com>
2026-08-15 00:07:21 -0600

    removed hyprland (moved to main) add module

---
+++ .config/hypr/modules/monitors.lua
@@ -0,0 +1,39 @@
+------------------
+---- MONITORS ----
+------------------
+
+hl.monitor({
+	output = "DP-1",
+	mode = "2560x1440",
+	position = "0x0",
+	scale = 1,
+})
+
+hl.monitor({
+	output = "DVI-D-1",
+	mode = "1440x900",
+	position = "2560x300",
+	scale = 1,
+})
+
+--------------------
+---- WORKSPACES ----
+--------------------
+
+-- Assign workspaces 1-7 to DP-1
+for i = 1, 7 do
+	hl.workspace_rule({
+		workspace = tostring(i),
+		monitor = "DP-1",
+		default = (i == 1),
+	})
+end
+
+-- Assign workspaces 6-10 to DVI-D-1
+for i = 8, 10 do
+	hl.workspace_rule({
+		workspace = tostring(i),
+		monitor = "DVI-D-1",
+		default = (i == 6),
+	})
+end
